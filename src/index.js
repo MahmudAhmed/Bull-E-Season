2f3eeced59569c6dfda9fdbd2eefcf0320470151
MahmudAhmed <moe.purplefox@gmail.com>
2020-03-05 12:06:50 -0500

    locked cars

--- src/index.js
+++ src/index.js
@@ -9,14 +9,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const leftBtn = document.getElementById("left-icon")
   const rightBtn = document.getElementById("right-icon")
   const playableCars = document.querySelectorAll(".playable-car")
-  
+  const highScore = document.getElementById("high-score");
+  const topScore = localStorage.getItem("highScore") || 0;
+  highScore.innerText = topScore;
+  const no = new Audio("./dist/no.mp3");
+
+
   let i = 0;
   playableCars[i].style.display = "flex";
   
   rightBtn.onclick = function(){
+    debugger
     playableCars[i].style.display = "";
     if (i === playableCars.length - 1) i = -1;
     i++;
+    if ((i === 3 || i === 4) && topScore >= 2000) document.querySelector(`.locked${i + 1}`).style.display = "none";
+    if ((i === 5) && topScore >= 3000) document.querySelector(`.locked${i+1}`).style.display = "none";
     playableCars[i].style.display = "flex";
   }
 
@@ -29,6 +37,14 @@ window.addEventListener("DOMContentLoaded", () => {
   let game;
   // debugger
   startGame.onclick = function(){
+    if ((i === 3 || i === 4) && topScore < 2000){
+      no.play();
+      return;
+    }
+    if ((i === 5) && topScore < 3000){
+      no.play();
+      return;
+    }
     startGame.style.display = "none";
     leftBtn.style.display = "none";
     rightBtn.style.display = "none";
